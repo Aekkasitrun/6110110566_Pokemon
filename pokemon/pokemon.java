d936806680f416c758e54bf56489dc433fd44848
Aekkasitrun <aekkasitrun@gmail.com>
2020-02-12 15:07:54 +0700

    beta1

---
+++ pokemon/pokemon.java
@@ -0,0 +1,15 @@
+package pokemon;
+import java.util.*;
+public class pokemon{
+    public static void main(String[] args){
+        Scanner InputPerson = new Scanner(System.in);
+    //set name player
+        System.out.println("Enter Your Name:");
+        String NamePerson = InputPerson.next();
+
+        System.out.println("Your name is:" + NamePerson);
+
+    }
+
+
+}
